e3c40182e02d6e0f1fb96737d1cbde9fe3752471
tangxingchu <51697550@qq.com>
2019-11-27 17:44:37 +0800

    提交

---
+++ src/views/syscategory/design.vue
@@ -0,0 +1,19 @@
+<template >
+  <el-container>
+    <EditorFormAside style="width:380px;"></EditorFormAside>
+    <EditorFormMain></EditorFormMain>
+  </el-container>
+</template>
+
+<script>
+import EditorFormAside from '../editor/form/aside';
+import EditorFormMain from '../editor/form/main';
+
+export default {
+  components: {
+	  EditorFormAside,
+	  EditorFormMain,
+  },
+
+};
+</script>
